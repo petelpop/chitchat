460b0e2637b0edeec58ad09d79b26acef115f556
ipal <72238450+petelpop@users.noreply.github.com>
2023-06-02 10:37:53 +0700

    send message  design

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,5 +1,9 @@
 <div>
-<div class="chatbox_footer">
-  footer
-</div>
+    <div class="chatbox_footer">
+
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="write message...">
+                <button class="submit" type="submit">Send</button>
+        </div>
+    </div>
 </div>
