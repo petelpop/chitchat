9c7a33b920edc0e54a840460f5aca0cbf2498160
ipal <72238450+petelpop@users.noreply.github.com>
2023-06-06 14:46:00 +0700

    show all conversation

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,4 +1,5 @@
 <div>
+    
     <div class="chatlist_header">
         <div class="title">
             Chat
@@ -9,26 +10,24 @@
         </div>
     </div>
 
+    @if (count($conversations) > 0)
+
+    @foreach ($conversations as $conversation)
+ 
     <div class="chatlist_body">
         <div class="chatlist_item">
             <div class="chatlist_img_container">
-                <img src="https://picsum.photos/id/227/200/300" alt="">
+                <img src="https://picsum.photos/id/{{ $this->getChatUserInstance($conversation, $name = 'id') }}/200/300" alt="">
             </div>
             
             <div class="chatlist_info">
                 <div class="top_row">
-                    <div class="list_username">zirjy</div>
-                    <span class="date">2d</span>
+                    <div class="list_username">{{ $this->getChatUserInstance($conversation, $name = 'name') }}</div>
+                    <span class="date">{{ $conversation->messages->last()->created_at->shortAbsoluteDiffForHumans() }}</span>
                 </div>
                 <div class="bottom_row">
                     <div class="message_body text-truncate">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore assumenda iure atque at! Odio,
-                        voluptatum commodi. Possimus sunt distinctio perferendis qui quas nulla dignissimos non dolor,
-                        voluptate rerum eos odio.
-
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus cumque quasi, dolore rem
-                        laudantium soluta. Fugit, ratione! Dolor suscipit pariatur reiciendis quod? Delectus, cum
-                        aliquam. Iusto rerum atque aliquid veritatis.
+                        {{ $conversation->messages->last()->body }}
                     </div>
 
                     <div class="unread_count">
@@ -38,4 +37,10 @@
             </div>
         </div>
     </div>
+
+    @endforeach
+    @else 
+        You have no conversations
+    @endif
+
 </div>
